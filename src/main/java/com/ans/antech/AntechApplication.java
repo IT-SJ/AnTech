a48d0325622a5d3371ea1a9ddb7293e9cf29fe3a
mone652 <rhdudqls652@gmail.com>
2025-01-23 14:38:46 +0900

    서버 오류

--- src/main/java/com/ans/antech/AntechApplication.java
+++ src/main/java/com/ans/antech/AntechApplication.java
@@ -1,9 +1,11 @@
 package com.ans.antech;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.ans.antech.mapper")
 public class AntechApplication {
 
 	public static void main(String[] args) {
