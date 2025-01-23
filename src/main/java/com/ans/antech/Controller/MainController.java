d7c99fe409ee0e23ed6a99ee58e26a3e87d95e8b
IT-SJ <asj30816766a@gamil.com>
2025-01-23 14:39:22 +0900

    커밋

--- src/main/java/com/ans/antech/Controller/MainController.java
+++ src/main/java/com/ans/antech/Controller/MainController.java
@@ -1,4 +1,4 @@
-package com.ans.antech.Controller;
+package com.ans.antech.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
