da3fccc12544c2dd68100583ca78b6d600b1570b
mone652 <rhdudqls652@gmail.com>
2025-01-25 17:30:02 +0900

    회원가입 아이디 중복,이메일 인증 완료2

--- src/main/java/com/ans/antech/service/MemberService.java
+++ src/main/java/com/ans/antech/service/MemberService.java
@@ -16,7 +16,6 @@ public class MemberService {
 
     // 회원가입
     public void registerMember(Member member) {
-        System.out.println("여기 서비스임: "+member.toString());
         mapper.insertMember(member);
     }
 
