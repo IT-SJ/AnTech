fe10789748eb8cf353291d4e989a282e502099a9
IT-SJ <asj30816766a@gamil.com>
2025-01-31 16:57:49 +0900

    (FE,BE) 성창섭의 정 상 화

--- src/main/java/com/ans/antech/mapper/NewsMapper.java
+++ src/main/java/com/ans/antech/mapper/NewsMapper.java
@@ -19,12 +19,15 @@ public interface NewsMapper {
 
     // 은진 요약(smr)을 기본으로 사용하고 부족하면 본문(text)을 반환 (워드클라우드 구현)
     List<String> getProcessedNewsContent();  
+
     // 메인 뉴스(+갯수)
     public int countNews();
+
     public List<News> findNewsByPage(int pageSize, int offset);
     
     // 속보 뉴스(+갯수)
     public int countBNews();
+    
     public List<News> findBNewsByPage(int pageSize, int offset);
     
     // 검색된 뉴스 개수 조회
